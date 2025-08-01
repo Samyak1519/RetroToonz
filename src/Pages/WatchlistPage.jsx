83b69aafbb6ffbf8a2c74a8220c1696158c4ceda
Samyak1519 <samnimsarkar@gmail.com>
2025-08-01 15:48:17 +0530

    Updated Search Logic and Search Page

--- src/Pages/WatchlistPage.jsx
+++ src/Pages/WatchlistPage.jsx
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 import { FaArrowLeft } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-=======
->>>>>>> 3ea8f468a04a22cbae3c34b1da6d24830d81c8ba
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 
 function WatchlistPage() {
-<<<<<<< HEAD
   const navigate = useNavigate();
 
   return (
@@ -34,17 +30,6 @@ function WatchlistPage() {
 
       <Footer />
     </div>
-=======
-  return (
-    <>
-      <Header />
-      <div className="pt-24 min-h-screen text-white px-4">
-        <h1 className="text-3xl font-bold mb-4">Your Watchlist</h1>
-        <p className="text-gray-400">Your saved shows will appear here.</p>
-      </div>
-      <Footer />
-    </>
->>>>>>> 3ea8f468a04a22cbae3c34b1da6d24830d81c8ba
   );
 }
 
