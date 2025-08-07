a1c351f457849903fb59e0f7d1dc1c54545501a6
Samyak1519 <samnimsarkar@gmail.com>
2025-08-07 18:48:24 +0530

    Added: Added new shows, and fixed search bar.

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -24,11 +24,11 @@ function SearchBar() {
         onChange={(e) => setQuery(e.target.value)}
         onKeyDown={handleKeyDown}
         placeholder="Search your favourite cartoon here..."
-        className="flex-grow border placeholder-opacity-50 placeholder-cyan-500 pl-5 border-cyan-700 rounded-l-3xl p-3 bg-transparent text-white outline-none"
+        className="flex-grow border placeholder-opacity-50 placeholder-cyan-500 pl-3.5 border-cyan-700 rounded-l-3xl p-2 bg-transparent text-white outline-none"
       />
       <button
         onClick={handleSearch}
-        className="p-2 px-4 text-cyan-500 border border-l-0 border-cyan-700 rounded-r-3xl hover:bg-gray-700"
+        className="p-2 px-3 text-cyan-500 border border-l-0 border-cyan-700 rounded-r-3xl hover:bg-gray-700"
       >
         Search
       </button>
